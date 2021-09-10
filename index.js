3c602353a3114f341da04e188ac489d347264248
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-09-10 17:40:57 +0300

    added footer icons

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
         $('#manage-managet').toggle(100)
     })
 });
+//verification
+function verify(){
+    let name = document.getElementById('idname').value;
+    let email = document.getElementById('idemail').value;
+    if(name==''){
+        alert('input valid name');
+        return false
+    }
+    if(email==''){
+        alert('input valid email');
+        return false
+    }
+alert('welcome '+name)
+alert('email is noted')
+}
