3a142f98957d92c0af9b60328f654df66a68edfb
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-09-11 15:53:28 +0300

    added overlay mouseout to work1 to work8

--- index.js
+++ index.js
@@ -47,6 +47,41 @@ $(document).ready(function(){
       $("#overlay6").hide();
     });
   });
+  $(document).ready(function(){
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
 
   $(document).ready(function(){
     $("form#formsubmit").submit(function(event){
